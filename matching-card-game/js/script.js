75985b8cd663aa0ad0e17f1bd3d6403a2fac443f
Ross Nelson <rdnelson2010@gmail.com>
2025-01-21 19:03:07 -0500

    my own changes

--- matching-card-game/js/script.js
+++ matching-card-game/js/script.js
@@ -20,11 +20,55 @@
 
 */
 
-const cards = [
-    {
-        src: '',
-        matched: false
-    }, {}, {}
+const cardImages = [
+    {src: "./assets/frog_394697.png", matched: false},
+    {src: "./assets/frog_394697.png", matched: false},
+    {src: "./assets/hamster_5389261.png", matched: false},
+    {src: "./assets/hamster_5389261.png", matched: false},
+    {src: "./assets/koala_3069172.png", matched: false},
+    {src: "./assets/koala_3069172.png", matched: false},
+    {src: "./assets/frog_394697.png", matched: false}
 ]
 
-const shuffledCards = [...cards, ...cards]
+
+//the ... is pointing out anyof the arrays in the cardImages array
+const cardArray = [...images, ...images]
+console.log("cardArray = ", cardImages)
+
+
+//DOM elements
+const cards = document.querySelectorAll('.card');
+const cardFaces = document.querySelectorAll('.face');
+
+
+//renderCard function
+function renderCard() {
+        // update each cardFace with the corresponding image from card
+        cardFaces.forEach((cardFaces, index) => {
+            const img =document.createElement('img')
+            img.src = cardArray[index].src
+            cardFaces.appendChild(img)
+        })
+}
+
+// shuffleCard function
+function shuffleCard() {
+    for(let i =cardImages.length - 1; i>0;i--) {
+        const j = Math.floor(Math.random() * (i+1))
+        const temp = cardImages[i];
+        [cardImages[i],cardImages[j]] = [cardImages[j],cardImages[i]]
+        console.log('${i},th iteration at the loop',cardImages);
+    }
+
+    return cardImages;
+
+}
+
+
+shuffleCard();
+renderCard();
+
+
+
+
+
